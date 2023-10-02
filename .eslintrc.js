89417bfed7e6eeb265ee2939f2273b443d29a4c5
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-10-03 00:00:32 +0100

    feat: bootstrap
    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// /* eslint-env node */
+// module.exports = {
+//   parser: "@typescript-eslint/parser",
+//   plugins: ["@typescript-eslint", "prettier"],
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:prettier/recommended",
+//   ],
+//   rules: {
+//     "prettier/prettier": [
+//       "error",
+//       {
+//         endOfLine: "auto",
+//       },
+//     ],
+//   },
+// };
